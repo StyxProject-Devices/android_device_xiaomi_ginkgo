e4143575c6992b2d362ec2a531b52de906319577
Anton Hansson <hansson@google.com>
2018-11-07 16:10:05 +0000

    g4w4: Enforce RRO for all resource overlays
    Bug: 118823052
    Test: make
    Change-Id: I579d09480c7a482543ea616dd5c493d825bb98b1

--- device.mk
+++ device.mk
@@ -15,6 +15,8 @@ $(call inherit-product, frameworks/native/build/phone-xhdpi-4096-dalvik-heap.mk)
 DEVICE_PACKAGE_OVERLAYS += \
     $(LOCAL_PATH)/overlay
 
+PRODUCT_ENFORCE_RRO_TARGETS := *
+
 PRODUCT_PACKAGES += \
     GinkgoMiuiCamera \
     NoCutoutOverlay \
